dcf002dac7ce5f55fac06e66402b9e0966f31d95
harpreetvirkk <harpreetvirk2018@gmail.com>
2018-12-03 04:15:53 +0530

    update

--- supplier_menu.c
+++ supplier_menu.c
@@ -4,18 +4,18 @@
 struct supplier{
     int supplier_id;
     char supplier_name[30];
-    char supplier_city[30];
-    char supplier_contact[13];
-    char supplier_email[40];
+    char supplier_city[20];
+    int supplier_contact;
+    char supplier_email[50];
 };
-sup_search_byid(){
+void sup_search_byid(){
     printf("Enter the supplier ID to find details!\n");
     int temp_id;
     struct supplier temp;
-    scanf("%d", temp_id);
+    scanf("%d", &temp_id);
     FILE *f=fopen("supplier.txt", "a+");
     rewind(f);
-    while(fscanf(f,"%d \n %s\n %s\n %d\n %s\n", temp.supplier_id, temp.supplier_name, temp.supplier_city, temp.supplier_contact, temp.supplier_email)!=EOF){
+    while(fscanf(f,"%d \n %s\n %s\n %d\n %s\n", &temp.supplier_id, temp.supplier_name, temp.supplier_city, &temp.supplier_contact, temp.supplier_email)!=EOF){
         if(temp_id==temp.supplier_id){
             printf("Supplier Found!\n");
             printf("Supplier id: %d\n", temp.supplier_id);
@@ -24,18 +24,21 @@ sup_search_byid(){
             printf("Supplier contact: %d\n", temp.supplier_contact);
             printf("Supplier email: %s\n", temp.supplier_email);
         }
+        else{
+            printf("Supplier does not exist!\n");
+        }
     }
     fclose(f);
     return;
 }
-sup_search_byname(){
+void sup_search_byname(){
     printf("Enter the supplier name to find details!\n");
-    char temp_name[30];
+    char *temp_name=malloc(sizeof(char*));
     struct supplier temp;
-    scanf("%d", &temp_name);
+    scanf("%s", temp_name);
     FILE *f=fopen("supplier.txt", "a+");
     rewind(f);
-    while(fscanf(f,"%d \n %s\n %s\n %d\n %s\n", temp.supplier_id, temp.supplier_name, temp.supplier_city, temp.supplier_contact, temp.supplier_email)!=EOF){
+    while(fscanf(f,"%d \n %s\n %s\n %d\n %s\n", &temp.supplier_id, temp.supplier_name, temp.supplier_city, &temp.supplier_contact, temp.supplier_email)!=EOF){
         if(strcmp(temp_name, temp.supplier_name)==0){
             printf("Supplier Found!\n");
             printf("Supplier id: %d\n", temp.supplier_id);
@@ -51,8 +54,8 @@ sup_search_byname(){
 void search_supplier(){
     int sup_search_choice;
     do{
-    printf("Enter 1 to search supplier by id!\n Enter 2 to search supplier by name!\n");
-    scanf("%d", sup_search_choice);
+    printf("Enter 1 to search supplier by id!\nEnter 2 to search supplier by name!\nEnter 3 to Exit!\n");
+    scanf("%d", &sup_search_choice);
     switch(sup_search_choice){
         case 1: sup_search_byid();
         break;
@@ -70,13 +73,13 @@ void create_supplier(){
     printf("Enter the ID for the new supplier! \n");
     scanf("%d", &temp.supplier_id);
     printf("Enter the name of the supplier! \n");
-    scanf("%s", &temp.supplier_name);
+    scanf("%s", temp.supplier_name);
     printf("Enter the city of the supplier! \n");
-    scanf("%s", &temp.supplier_city);
+    scanf("%s", temp.supplier_city);
     printf("Enter the contact number of the supplier! \n");
     scanf("%d", &temp.supplier_contact);
     printf("Enter the email id of the supplier! \n");
-    scanf("%s", &temp.supplier_email);
+    scanf("%s", temp.supplier_email);
     fprintf(fp, "%d \n %s\n %s\n %d\n %s\n", temp.supplier_id, temp.supplier_name, temp.supplier_city, temp.supplier_contact, temp.supplier_email);
     fclose(fp);
     return;
@@ -89,7 +92,7 @@ int main(void){
 int supplier_menu_choice;
 
 do{
-	printf("Enter 1 to search supplier. \nEnter 2 to create new supplier. \nEnter 3 to update supplier information. \nEnter 4 to go back to Main Menu.");
+	printf("Enter 1 to search supplier. \nEnter 2 to create new supplier. \nEnter 3 to update supplier information. \nEnter 4 to go back to Main Menu.\n");
 	scanf("%d",&supplier_menu_choice);
 
 switch(supplier_menu_choice)
