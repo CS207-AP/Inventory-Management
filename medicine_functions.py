10f352ccb530bf05d1dc6300f2108cf3ea08208c
virjhangiani <vir.jhangiani_ug20@ashoka.edu.in>
2018-12-05 05:04:38 +0530

    added new function

--- medicine_functions.py
+++ medicine_functions.py
@@ -1,9 +1,10 @@
 import csv
-def add_medicine:
+def add_medicine():
 	with open('medicine.csv','a+') as csvfile:
 		columns = ['medi_name','med_id','sale','unit','quantity','min_quantity', 'exp_date',\
 		'pur_date','comp_name', 'supp_name','cost','total','to_pur']
 		writer = csv.DictWriter(csvfile,fieldnames = columns)
+		
 		medi_name = input("Enter medicine name:")
 		med_id = input("Enter ID:")
 		sale = float(input("Enter sale price:"))
@@ -22,3 +23,12 @@ def add_medicine:
 		writer.writerow({'medi_name':medi_name,'med_id':med_id,'sale':sale,'unit':unit,'quantity':quantity,\
 		'min_quantity':min_quantity,'exp_date':exp_date,'pur_date':pur_date,'comp_name':comp_name, \
 		'supp_name':supp_name,'cost':cost,'total':total,'to_pur':to_pur})
+
+def search_medicine():
+    with open('medicine.csv','r') as csvfile:
+        name=input('Enter the medicine to search:')
+        reader=csv.DictReader(csvfile)
+        for row in reader:
+            if row['medi_name'] == name:
+                print('Name:', row['medi_name'],'\n','Quantity:',row['quantity'],'\n','Price:',row['sale'])		
+              
