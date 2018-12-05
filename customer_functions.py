88aab9c559b415b4bc1c055a67634649af0a4fbd
virjhangiani <vir.jhangiani_ug20@ashoka.edu.in>
2018-12-05 06:01:37 +0530

    function names updated, customer update function created

--- customer_functions.py
+++ customer_functions.py
@@ -1,48 +1,61 @@
+import csv
+
+from tempfile import NamedTemporaryFile
+import shutil
+
+
+
+
 def new_customer():
-    import csv
+    
     with open('cus_men.csv','a+') as csvfile:
         names=['Customer_Name','Customer_ID','Customer_Phone','Customer_Medicine']
         writer=csv.DictWriter(csvfile,fieldnames=names)
         Customer_Name=input('Enter the name of the customer: ')
         Customer_ID=input('Enter the customer ID: ')
-        Customer_Phone=int(input('Enter the phone number of the customer: '))
+        Customer_Phone=(input('Enter the phone number of the customer: '))
         Customer_Medicine=input('Enter the medicine that the customer needs: ')
         writer.writerow({'Customer_Name':Customer_Name,'Customer_ID':Customer_ID,'Customer_Phone':Customer_Phone,"Customer_Medicine":Customer_Medicine})
 
-def customer_search():
-    import csv
+def search_customer():
+    
     with open('cus_men.csv','r') as csvfile:
         no=input('Enter the id to search ')
         reader=csv.DictReader(csvfile)
         for row in reader:
             if row['Customer_ID']==no:
                 print(row['Customer_Name'],row['Customer_ID'],row['Customer_Phone'],row['Customer_Medicine'])
-
-def customer_update():
-    import csv
-    with open('cus_men.csv','r+') as csvfile:
-        names=['Customer_Name','Customer_ID','Customer_Phone','Customer_Medicine']
-        writer=csv.DictWriter(csvfile,fieldnames=names)    
-        no=input('Enter the id of the customer you want to modify')
-        reader=csv.DictReader(csvfile)
+def update_customer_info():
+    tempfile = NamedTemporaryFile(mode='w', delete=False)
+    names=['Customer_Name','Customer_ID','Customer_Phone','Customer_Medicine']
+    with open('cus_men.csv', 'r') as csvfile, tempfile:
+        reader = csv.DictReader(csvfile)
+        writer = csv.DictWriter(tempfile, fieldnames=names)
+        writer.writeheader()
+        idno =input('Enter the id of the customer you want to modify')
         for row in reader:
-            if row['Customer_ID']==no:
-                choice=int(input('1. To update ID\n2. To update the Name\n3.To update the phone number\n4.To update the medicine name'))
+            if row['Customer_ID'] == idno:
+                choice=int(input('1. To update the Name\n2.To update the phone number\n3.To update the medicine name'))
+
                 if(choice==1):
-                    row['Customer_ID']= input("Enter new ID: ")
-                    writer.writerow({'Customer_Name':row['Customer_Name'],'Customer_ID':row['Customer_ID'],'Customer_Phone':row['Customer_Phone'],"Customer_Medicine":row['Customer_Medicine']})
-                    break
-                elif(choice==2):
                     row['Customer_Name']=input("Enter the new name: ")
-                    writer.writerow({'Customer_Name':row['Customer_Name'],'Customer_ID':row['Customer_ID'],'Customer_Phone':row['Customer_Phone'],"Customer_Medicine":row['Customer_Medicine']})
-                    break
-                elif(choice==3):
+
+                elif(choice==2):
                     row['Customer_Phone']=input("Enter the new phone number: ")
-                    writer.writerow({'Customer_Name':row['Customer_Name'],'Customer_ID':row['Customer_ID'],'Customer_Phone':row['Customer_Phone'],"Customer_Medicine":row['Customer_Medicine']})
-                    break
-                elif(choice==4):
+
+                elif(choice==3):
                     row['Customer_Medicine']=input("Enter the medicine name")
-                    writer.writerow({'Customer_Name':row['Customer_Name'],'Customer_ID':row['Customer_ID'],'Customer_Phone':row['Customer_Phone'],"Customer_Medicine":row['Customer_Medicine']})
-                    break
+
+
+
+            row = {'Customer_Name':row['Customer_Name'],'Customer_ID':row['Customer_ID'],'Customer_Phone':row['Customer_Phone'],"Customer_Medicine":row['Customer_Medicine']}
+            writer.writerow(row)
+
+    shutil.move(tempfile.name, 'cus_men.csv')
+
+
+
+    
+                
        
                    
